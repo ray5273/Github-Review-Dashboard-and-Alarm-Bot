e20d9ef775699cab8169b4a114e520d0afaf406f
Sanghyeok Park <ray5273@naver.com>
2023-12-06 21:28:39 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// This is a simple TypeScript index file
+
+function welcomeMessage(name: string): string {
+    return `Welcome to TypeScript, ${name}!`;
+}
+
+console.log(welcomeMessage("Developer"));
